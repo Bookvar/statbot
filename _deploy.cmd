90b7de8d1d01f9cb6105b16ffd62552d55fd380d
bookvar <vbnrtdbx@gmail.com>
2021-06-11 18:00:30 +0300

    cmd

---
+++ _deploy.cmd
@@ -0,0 +1,8 @@
+copy .env c:\bot\statbot\* /y
+copy ok_stats.pyw c:\bot\statbot\* /y
+copy tg_stats.pyw c:\bot\statbot\* /y
+copy vk_stats.pyw c:\bot\statbot\* /y
+copy yt_stats_firefox.pyw c:\bot\statbot\* /y
+copy sowa_firefox.pyw c:\bot\statbot\* /y
+TIMEOUT /T 3
+
